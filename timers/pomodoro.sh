dd0c3077710a32672b68a954349fe8f8485e65da
Tyler <7563352+tylerwarre@users.noreply.github.com>
2024-03-07 09:06:29 -0700

    Update pomodoro.sh
    Updated pommodoro timer to have short and long breaks

--- timers/pomodoro.sh
+++ timers/pomodoro.sh
@@ -2,8 +2,9 @@
 # ______________________________________________________________| locals |__ ;
 timer_type="pomodoro"
 timer_duration_minutes="60"
-timer_break_minutes="5"
-timer_auto_restart=false
+timer_short_break_minutes="5"
+timer_long_break_minutes="60"
+timer_sessions="2"
 timer_on="#[fg=#282828]#[bg=#427b58]#[bold] 󰥔"
 timer_complete="#[fg=#282828]#[bg=#427b58]#[bold] "
 timer_notifications="true"
