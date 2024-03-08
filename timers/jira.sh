916327db307be1b52d9ac6f8514c88e506232813
Tyler <7563352+tylerwarre@users.noreply.github.com>
2024-03-08 14:07:02 -0700

    Create jira.sh timer

---
+++ timers/jira.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# ______________________________________________________________| locals |__ ;
+timer_type="pomodoro-jira"
+timer_duration_minutes="60"
+timer_short_break_minutes="5"
+timer_long_break_minutes="60"
+timer_sessions="2"
+timer_on="#[fg=#282828]#[bg=#427b58]#[bold] 󰥔"
+timer_complete="#[fg=#282828]#[bg=#427b58]#[bold] "
+timer_notifications="true"
+timer_granularity="false"
+timer_sound="false"
+timer_start_completed="false"
