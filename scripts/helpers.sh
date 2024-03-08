65ca60b1e43b0af0a1693135f70b222eb9cdfdaf
Tyler <7563352+tylerwarre@users.noreply.github.com>
2024-03-08 14:04:54 -0700

    Added Timers_Dir to helpers.sh

--- scripts/helpers.sh
+++ scripts/helpers.sh
@@ -1,50 +1,49 @@
 #!/usr/bin/env bash
+TIMER_DIR="/tmp/tmux-timer/$TIMER_SELECTION"
 DEBUG_FILE=/tmp/timer-debug.log
 
 get_tmux_option() {
-	local option=$1
-	local default_value=$2
-	option_value=$(tmux show-option -gqv "$option")
-	if [ -z "$option_value" ]; then
-		echo "$default_value"
-	else
-		echo "$option_value"
-	fi
+    local option=$1
+    local default_value=$2
+    option_value=$(tmux show-option -gqv "$option")
+    if [ -z "$option_value" ]; then
+        echo "$default_value"
+    else
+        echo "$option_value"
+    fi
 }
 
 set_tmux_option() {
-	local option="$1"
-	local value="$2"
-	tmux set-option -g "$option" "$value"
+    local option="$1"
+    local value="$2"
+    tmux set-option -g "$option" "$value"
 }
 
 read_file() {
-	local file=$1
-	if [ -f "$1" ]; then
-		cat "$1"
-	else
-		echo -1
-	fi
+    local file=$1
+    if [ -f "$1" ]; then
+        cat "$1"
+    else
+        echo -1
+    fi
 }
 
 remove_file() {
-	local file=$1
-	if [ -f "$file" ]; then
-		rm "$file"
-	fi
+    local file=$1
+    rm $file
 }
 
 write_to_file() {
-	local data=$1
-	local file=$2
-	echo "$data" > "$file"
+    local data=$1
+    local file=$2
+    echo "$data" > "$file"
 }
 
 debug() {
-	debugging=$(get_tmux_option "@timers_debug" false)
-	if [ "$debugging" = true ]; then
-		local data=$1
-		local file=$2
-		echo "$data" >> "$file"
-	fi
+    debugging=$(get_tmux_option "@timers_debug" false)
+    if [ "$debugging" = true ]; then
+        local data=$1
+        local file=$2
+        echo "$data" >> "$file"
+    fi
 }
